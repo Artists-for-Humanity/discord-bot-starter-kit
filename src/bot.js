0f4b889dc9fcec86607b71d3138cac0a7f73673c
Stephen Cronin <cronin4392@gmail.com>
2021-01-20 16:05:21 -0500

    Rename index.js to bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,6 @@
+/*
+  START: You can ignore this
+*/
 require("dotenv").config();
 
 if (!process.env.BOT_TOKEN) {
@@ -9,24 +12,28 @@ if (!process.env.CHANNEL_ID) {
   console.log("Please create an .env file with a CHANNEL_ID");
   return;
 }
+/*
+  END: You can ignore this
+*/
 
 const { Client, MessageEmbed } = require("discord.js");
 
-// Set up the Discord plugin.
 const client = new Client();
 const CHANNEL_ID = process.env.CHANNEL_ID;
 
 client.login(process.env.BOT_TOKEN);
+
+// Events
 client.on("ready", readyDiscord);
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
 client.on("message", messageRecieved);
 
-async function readyDiscord() {
+function readyDiscord() {
   console.log(`${client.user.username} ready!`);
 }
 
-async function messageRecieved(message) {
+function messageRecieved(message) {
   if (message.channel.id == CHANNEL_ID) {
     console.log(`Message Received: ${message.content}`);
   }
